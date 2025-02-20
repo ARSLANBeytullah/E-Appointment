1ba45d68f505039c3344d5b83647cd2da325a90e
ARSLANBeytullah <arslanbeytullah5800@gmail.com>
2025-02-21 00:31:44 +0300

    Add login command and handler with project reference
    Updated `eAppointment.Application.csproj` to include a project reference to `eAppointment.Domain.csproj`. Added `LoginCommand.cs` defining a `LoginCommand` record with `UserNameOrEmail` and `Password` properties, implementing `IRequest`. Added `LoginCommandHandler.cs` implementing `IRequestHandler` to handle `LoginCommand` requests using `UserManager<AppUser>`. Added `LoginCommandResponse.cs` defining a `LoginCommandResponse` record with a `Token` property.

---
+++ eAppointment.Application/Features/Auth/Login/LoginCommand.cs
@@ -0,0 +1,8 @@
+﻿using MediatR;
+using TS.Result;
+
+namespace eAppointment.Application.Features.Auth.Login;
+
+public sealed record LoginCommand(
+    string UserNameOrEmail,
+    string Password) : IRequest<Result<LoginCommandResponse>>; //Result objesi geriye LoginCommandResponse döndürmüş olacak.
