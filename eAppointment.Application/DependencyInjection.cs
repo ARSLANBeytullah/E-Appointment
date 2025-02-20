7c5051eba2c2193a7e34bfbcb2c22fd34907b86a
ARSLANBeytullah <arslanbeytullah5800@gmail.com>
2025-02-20 23:48:28 +0300

    The MediatR library has been added to the project and integrated into the relevant layer using dependency injection.

--- eAppointment.Application/DependencyInjection.cs
+++ eAppointment.Application/DependencyInjection.cs
@@ -6,8 +6,11 @@ public static  class DependencyInjection
 {
     public static IServiceCollection AddApplication(this IServiceCollection services)
     {
+        services.AddMediatR(configuration =>
+        {
+            configuration.RegisterServicesFromAssembly(typeof(DependencyInjection).Assembly);
+        });
         //services.AddAutoMapper(typeof(DependencyInjection).Assembly);
-        //services.AddMediatR(typeof(DependencyInjection).Assembly);
         //services.AddValidatorsFromAssembly(typeof(DependencyInjection).Assembly);
         //services.AddGenericRepository();
         //services.AddScoped<IAppointmentRepository, AppointmentRepository>();
