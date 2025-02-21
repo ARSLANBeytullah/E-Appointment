bed6f50ee0e5f3ce11fe1d0b09bb86c8a1953832
ARSLANBeytullah <arslanbeytullah5800@gmail.com>
2025-02-21 21:44:00 +0300

    Add eAppointmentServer.Infrastructure project
    Introduce new eAppointmentServer.Infrastructure project to the solution.
    - Add `ApplicationDbContext` class inheriting from `IdentityDbContext`.
    - Create `DependencyInjection` class with `AddInfrastructure` method.
    - Configure new project targeting .NET 8.0 with necessary package references.
    - Update solution file and build configurations for Debug and Release modes.

---
+++ eAppointmentServer.Infrastructure/Context/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using eAppointment.Domain.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace eAppointmentServer.Infrastructure.Context;
+
+internal sealed class ApplicationDbContext : IdentityDbContext<AppUser, AppRole, Guid>
+{
+    public ApplicationDbContext(DbContextOptions options) : base(options)
+    {
+        
+    }
+
+
+
+}
---
+++ eAppointmentServer.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,31 @@
+﻿using eAppointment.Domain.Entities;
+using eAppointmentServer.Infrastructure.Context;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace eAppointmentServer.Infrastructure;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructure(
+        this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        services.AddDbContext<ApplicationDbContext>(optionsAction: options =>
+        {
+            options.UseSqlServer(configuration.GetConnectionString("SqlServer"));
+        });
+
+        services.AddIdentity<AppUser, AppRole>(action =>
+        {
+            action.Password.RequiredLength = 3;
+            action.Password.RequireUppercase = false;
+            action.Password.RequireLowercase = false;
+            action.Password.RequireNonAlphanumeric = false;
+            action.Password.RequireDigit = false;
+        }).AddEntityFrameworkStores<ApplicationDbContext>();     
+        return services;
+    }
+
+}
