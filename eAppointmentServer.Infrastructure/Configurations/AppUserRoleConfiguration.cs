f3eb4216295a61cc8b78544d7999f6f36c662f6a
ARSLANBeytullah <arslanbeytullah5800@gmail.com>
2025-02-22 21:53:40 +0300

    Add new configurations and migration for AppUserRole
    Updated DoctorConfiguration.cs to include a new using directive for eAppointment.Domain.Enums and added a property configuration for Department with value conversion for DepartmentEnum. Modified ApplicationDbContext.cs to include additional generic parameters for IdentityUserClaim<Guid>, AppUserRole, IdentityUserLogin<Guid>, IdentityRoleClaim<Guid>, and IdentityUserToken<Guid>. Updated appsettings.json to add a new section for ConnectionStrings with a SqlServer connection string. Added a new package reference for Microsoft.EntityFrameworkCore.Design version 8.0.3 in eAppointmentServer.WebAPI.csproj. Introduced a new configuration class AppUserRoleConfiguration to configure the AppUserRole entity with a composite key. Added a new migration file 20250222185136_mg1.cs to create tables for Doctors, Patients, Roles, UserRoles, Users, and Appointments, including foreign key constraints and indexes. Added 20250222185136_mg1.Designer.cs to auto-generate the model snapshot for the migration. Updated ApplicationDbContextModelSnapshot.cs to include the new entities and their configurations.

---
+++ eAppointmentServer.Infrastructure/Configurations/AppUserRoleConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using eAppointment.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eAppointmentServer.Infrastructure.Configurations
+{
+    internal class AppUserRoleConfiguration : IEntityTypeConfiguration<AppUserRole>
+    {
+        public void Configure(EntityTypeBuilder<AppUserRole> builder)
+        {
+            builder.HasKey(k => new { k.UserId, k.RoleId });//composite key
+        }
+    }
+}
--- eAppointmentServer.Infrastructure/Configurations/DoctorConfiguration.cs
+++ eAppointmentServer.Infrastructure/Configurations/DoctorConfiguration.cs
@@ -1,4 +1,5 @@
 ﻿using eAppointment.Domain.Entities;
+using eAppointment.Domain.Enums;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
@@ -10,6 +11,10 @@ internal sealed class DoctorConfiguration : IEntityTypeConfiguration<Doctor>
         {
             builder.Property(p => p.FirstName).HasColumnType("varchar(50)");
             builder.Property(p => p.LastName).HasColumnType("varchar(50)");
-            //builder.HasIndex(p => new { p.FirstName, p.LastName}).IsUnique();
-        }
+        //builder.HasIndex(p => new { p.FirstName, p.LastName}).IsUnique();
+
+        builder.Property(p => p.Department)
+            .HasConversion(v => v.Value, v => DepartmentEnum.FromValue(v))
+            .HasColumnName("Department");
+    }
     }
